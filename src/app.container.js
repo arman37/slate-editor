3a2eb87d841a53cf03d59c3604d52640f45fde9c
arman37 <arman.it37@gmail.com>
2018-08-21 00:28:43 +0600

    Imported necessary dependencies

--- src/app.container.js
+++ src/app.container.js
@@ -6,6 +6,49 @@
 'use strict';
 
 import React from 'react';
+import isUrl from 'is-url';
+import styled from 'react-emotion';
+import { Block, Value } from 'slate';
+import initialValue from './value.json';
+import App from './app.component';
+import { isKeyHotkey } from 'is-hotkey';
+import imageExtensions from 'image-extensions';
+import { Button, Icon, Toolbar } from './components';
+import { LAST_CHILD_TYPE_INVALID } from 'slate-schema-violations';
+import { Editor, getEventRange, getEventTransfer } from 'slate-react';
+
+
+const DEFAULT_NODE = 'paragraph';
+
+const isBoldHotkey = isKeyHotkey('mod+b');
+const isItalicHotkey = isKeyHotkey('mod+i');
+const isUnderlinedHotkey = isKeyHotkey('mod+u');
+const isCodeHotkey = isKeyHotkey('mod+`');
+
+const Image = styled('img')`
+  display: block;
+  max-width: 100%;
+  max-height: 20em;
+  box-shadow: ${props => (props.selected ? '0 0 0 2px blue;' : 'none')};
+`;
+
+function isImage(url) {
+  return !!imageExtensions.find(url.endsWith)
+}
+
+const schema = {
+  document: {
+    last: { type: 'paragraph' },
+    normalize: (change, reason, { node, child }) => {
+      switch (reason) {
+        case LAST_CHILD_TYPE_INVALID: {
+          const paragraph = Block.create('paragraph')
+          return change.insertNodeByKey(node.key, node.nodes.size, paragraph)
+        }
+      }
+    },
+  },
+};
 
 class AppContainer extends React.Component {
   constructor(props) {
@@ -14,7 +57,6 @@ class AppContainer extends React.Component {
       value: Value.fromJSON(initialValue)
     };
   }
-
 
 }
 
