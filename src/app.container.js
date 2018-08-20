4c589b40ab285faa8a088021a2bea18227d89982
arman37 <arman.it37@gmail.com>
2018-08-21 00:54:32 +0600

    Implemented renderNode handler

--- src/app.container.js
+++ src/app.container.js
@@ -65,7 +65,35 @@ class AppContainer extends React.Component {
   onChange = ({ value }) => {
     this.setState({ value });
   };
-  
+
+  renderNode = props => {
+    const { attributes, children, node, isFocused } = props;
+
+    switch (node.type) {
+      case 'block-quote':
+        return <blockquote {...attributes}>{children}</blockquote>;
+
+      case 'bulleted-list':
+        return <ul {...attributes}>{children}</ul>;
+
+      case 'heading-one':
+        return <h1 {...attributes}>{children}</h1>;
+
+      case 'heading-two':
+        return <h2 {...attributes}>{children}</h2>;
+
+      case 'list-item':
+        return <li {...attributes}>{children}</li>;
+
+      case 'numbered-list':
+        return <ol {...attributes}>{children}</ol>;
+
+      case 'image': {
+        const src = node.data.get('src');
+        return <Image src={src} selected={isFocused} {...attributes} />
+      }
+    }
+  };
 
   /****************************************** Image Specific Handlers*******************************************/
   _handleImageUpload = () => {
