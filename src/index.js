d280c773638546a2781b3e5c6f344f4e56796c56
arman37 <arman.it37@gmail.com>
2018-08-20 23:40:26 +0600

    Added app component with index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+/**
+ * @author arman
+ * @since 8/20/18
+ *
+ */
+'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import App from './app';
 
 const title = 'A custom document editor ';
 
 ReactDOM.render(
   <div>
-    {title}
+    <App />
   </div>,
   document.getElementById('app')
 );
