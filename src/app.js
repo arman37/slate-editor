13fd793ae0afc386a45f826acbd5637792b72e42
arman37 <arman.it37@gmail.com>
2018-08-20 23:23:04 +0600

    created app component

---
+++ src/app.js
@@ -0,0 +1,24 @@
+/**
+ * @author arman
+ * @since 8/20/18
+ *
+ */
+'use strict';
+
+import React from 'react';
+import AppContainer from './app.container';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <AppContainer />
+    );
+  }
+}
