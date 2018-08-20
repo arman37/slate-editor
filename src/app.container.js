de08a71ee08daaaa93db4d174961a7f630f81f27
arman37 <arman.it37@gmail.com>
2018-08-21 00:05:22 +0600

    Created app container component

---
+++ src/app.container.js
@@ -0,0 +1,21 @@
+/**
+ * @author arman
+ * @since 8/20/18
+ *
+ */
+'use strict';
+
+import React from 'react';
+
+class AppContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: Value.fromJSON(initialValue)
+    };
+  }
+
+
+}
+
+export default AppContainer;
